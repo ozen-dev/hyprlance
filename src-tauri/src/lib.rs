db704ae417419bce617a39b2c19d399374faf69a
Enzo <enzo.romera.fr@gmail.com>
2024-11-01 18:32:53 +0100

    Connect with contra

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,17 +1,24 @@
 mod platforms;
 
 use platforms::{AuthData, AuthService};
+use tauri::Emitter;
 
 #[tauri::command]
 async fn authenticate(app_handle: tauri::AppHandle, platform: &str) -> Result<AuthData, String> {
     AuthService::authenticate(app_handle, platform).await
 }
 
+#[tauri::command]
+async fn emit_api_data(app_handle: tauri::AppHandle, payload: &str) -> Result<(), ()> {
+    let _ = app_handle.emit("hyprlance:contra-data", payload);
+    Ok(())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![authenticate])
+        .invoke_handler(tauri::generate_handler![authenticate, emit_api_data])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
